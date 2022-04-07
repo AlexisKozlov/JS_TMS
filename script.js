57c9bcfdb7df5d3bcfc1a77889321134780657bc
AlexisKozlov <alex2000kozlov@gmail.com>
2022-04-07 18:23:54 +0300

    add code for homework

---
+++ script.js
@@ -0,0 +1,65 @@
+
+// // Task #1
+// let x = 20;
+// let y = 58;    
+// let z = 42;
+// let result = x + y +z;
+// alert(result);
+
+
+
+// Task #2
+// let min = 60;
+// let hour = 60;
+// let day = 24;
+// let year = 365;
+// let my = 18;
+
+// let myAgeInSeconds = my * year * day * hour * min;
+// alert(myAgeInSeconds);
+
+
+
+//Task #3
+// let count = 42    
+// let userName = '42' 
+// count = String(count);
+// alert(typeof count)
+// userName = Number(userName);
+// alert(typeof userName)
+
+
+//Task #4
+// let a = 1;
+// let b = 2;
+// let c = " белых медведей";
+// a = String(a);
+// b = String(b);
+// let result = a + b + c;
+// console.log (result)
+
+//Task #5
+// let a = 'доступ';
+// let b = 'морпех';
+// let c = 'наледь';
+// let d = 'попрек';
+// let i = 'руибло';
+
+// console.log (a.length + b.length + c.length + d.length + i.length)
+
+
+//Task #6
+// let a = 45;
+// console.log ("Variable:", "a", "have type:", typeof a)
+// let b = 'str';
+// console.log ("Variable:", "b", "have type:", typeof b)
+// let t = true;
+// console.log ("Variable:", "t", "have type:", typeof t)
+
+
+//Task #7
+// let userName = prompt("Введите своё имя")
+// console.log('имя: ', userName);
+// let userYears = prompt ("Укажите свой возраст")
+// console.log ('возраст: ', userYears)
+
